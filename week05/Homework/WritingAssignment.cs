32c7b1037c118b4dce4acc0305a59dc05b110def
Donaldson Tega <sogolodonald11@gmail.com>
2025-10-05 19:40:46 +0100

    Create WritingAssignment

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,16 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Uses base class method to access student name safely
+        return $"{_title} by {GetStudentName()}";
+    }
+}
